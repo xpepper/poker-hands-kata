acfa27ccabefc8565a97e0ab5fc9b4cf46a9286e
Pietro Di Bello <pierodibello@gmail.com>
2020-05-21 23:09:55 +0200

    returns a tie when there is no winning hand

--- src/test/java/com/kata/poker/Game.java
+++ src/test/java/com/kata/poker/Game.java
@@ -1,6 +1,10 @@
 package com.kata.poker;
 
 public class Game {
+    private static final int SECOND_HAND = -1;
+    private static final int FIRST_HAND = 1;
+    private static final int TIE = 0;
+
     private final PokerHand firstHand;
     private final PokerHand secondHand;
 
@@ -10,10 +14,15 @@ public class Game {
     }
 
     public String play() {
-        if (firstHand.compareTo(secondHand) == -1) {
-            return "second hand wins.";
-        } else {
-            return "first hand wins.";
+        int winningHand = firstHand.compareTo(secondHand);
+        switch (winningHand) {
+            case FIRST_HAND:
+                return "first hand wins.";
+            case SECOND_HAND:
+                return "second hand wins.";
+            case TIE:
+            default:
+                return "Tie.";
         }
     }
 }
--- src/test/java/com/kata/poker/GameTest.java
+++ src/test/java/com/kata/poker/GameTest.java
@@ -2,8 +2,7 @@ package com.kata.poker;
 
 import org.junit.Test;
 
-import static com.kata.poker.Card.Suit.Diamonds;
-import static com.kata.poker.Card.Suit.Hearts;
+import static com.kata.poker.Card.Suit.*;
 import static com.kata.poker.PokerHandTest.*;
 import static org.junit.Assert.assertEquals;
 
@@ -17,4 +16,13 @@ public class GameTest {
         assertEquals("first hand wins.", new Game(higherHand, hand).play());
         assertEquals("second hand wins.", new Game(hand, higherHand).play());
     }
+
+    @Test
+    public void returns_a_tie_when_there_is_no_winning_hand() {
+        PokerHand hand = new PokerHand(fiveOf(Diamonds), threeOf(Hearts));
+        PokerHand otherHand = new PokerHand(fiveOf(Clubs), threeOf(Spades));
+
+        assertEquals("Tie.", new Game(hand, otherHand).play());
+    }
+
 }
