0cec436140d53494f173ee33373e8106183586b1
Pietro Di Bello <pierodibello@gmail.com>
2020-08-05 22:56:00 +0200

    refactor test

--- src/test/java/com/kata/poker/GameResultFormatterTest.java
+++ src/test/java/com/kata/poker/GameResultFormatterTest.java
@@ -10,22 +10,24 @@ import static org.junit.Assert.assertThat;
 
 public class GameResultFormatterTest {
 
+    private final GameResultFormatter formatter = new GameResultFormatter();
+
     @Test
     public void format_the_winner_name_with_its_rank_and_point() {
         Winner winner = new Winner(new Player("Peter", new Hand(threeOf(Clubs), sevenOf(Hearts))));
 
-        assertEquals("Peter wins. - with high card: 7", new GameResultFormatter().format(winner));
+        assertEquals("Peter wins. - with high card: 7", formatter.format(winner));
     }
 
     @Test
     public void format_the_straight_rank() {
         Winner winner = new Winner(new Player("anyName", new Hand(fiveOf(Hearts), fourOf(Diamonds))));
 
-        assertThat(new GameResultFormatter().format(winner), containsString("straight: 5-high"));
+        assertThat(formatter.format(winner), containsString("straight: 5-high"));
     }
 
     @Test
     public void format_tie_result() {
-        assertEquals("Tie.", new GameResultFormatter().format(GameResult.tie));
+        assertEquals("Tie.", formatter.format(GameResult.tie));
     }
 }
