5ced7df04c1671c6f44b8195f5f55fcd20d7373f
Pietro Di Bello <pietro.dibello@xpeppers.com>
2020-11-18 23:00:24 +0100

    improve GameResultFormatterTest readability when building a winner...

--- src/test/java/com/kata/poker/GameResultFormatterTest.java
+++ src/test/java/com/kata/poker/GameResultFormatterTest.java
@@ -28,42 +28,42 @@ public class GameResultFormatterTest {
 
     @Test
     public void format_the_high_card_rank() {
-        Winner winner = new Winner(new Player("anyName", new Hand(threeOf(Clubs), fiveOf(Diamonds), sevenOf(Hearts))));
+        Winner winner = aWinnerWith(threeOf(Clubs), fiveOf(Diamonds), sevenOf(Hearts));
 
         assertThat(formatter.format(winner), containsString("high card: 7"));
     }
 
     @Test
     public void format_a_rank_with_a_figure() {
-        Winner winner = new Winner(new Player("anyName", new Hand(threeOf(Clubs), queenOf(Diamonds), aceOf(Hearts))));
+        Winner winner = aWinnerWith(threeOf(Clubs), queenOf(Diamonds), aceOf(Hearts));
 
         assertThat(formatter.format(winner), containsString("high card: Ace"));
     }
 
     @Test
     public void format_the_pair_rank() {
-        Winner winner = new Winner(new Player("anyName", new Hand(fiveOf(Hearts), fiveOf(Diamonds), threeOf(Clubs))));
+        Winner winner = aWinnerWith(fiveOf(Hearts), fiveOf(Diamonds), threeOf(Clubs));
 
         assertThat(formatter.format(winner), containsString("pair: 5s"));
     }
 
     @Test
     public void format_the_straight_rank() {
-        Winner winner = new Winner(new Player("anyName", new Hand(fiveOf(Hearts), fourOf(Diamonds), threeOf(Clubs))));
+        Winner winner = aWinnerWith(fiveOf(Hearts), fourOf(Diamonds), threeOf(Clubs));
 
         assertThat(formatter.format(winner), containsString("straight: 5-high"));
     }
 
     @Test
     public void format_the_flush_rank() {
-        Winner winner = new Winner(new Player("anyName", new Hand(fiveOf(Hearts), sevenOf(Hearts), threeOf(Hearts))));
+        Winner winner = aWinnerWith(fiveOf(Hearts), sevenOf(Hearts), threeOf(Hearts));
 
         assertThat(formatter.format(winner), containsString("flush: 7-high"));
     }
 
     @Test
     public void format_the_straight_flush_rank() {
-        Winner winner = new Winner(new Player("anyName", new Hand(fiveOf(Hearts), fourOf(Hearts), threeOf(Hearts))));
+        Winner winner = aWinnerWith(fiveOf(Hearts), fourOf(Hearts), threeOf(Hearts));
 
         assertThat(formatter.format(winner), containsString("straight flush: 5-high"));
     }
@@ -71,5 +71,9 @@ public class GameResultFormatterTest {
     @Test
     public void format_tie_result() {
         assertEquals("Tie.", formatter.format(GameResult.tie));
+    }
+
+    private Winner aWinnerWith(Card... cards) {
+        return new Winner(new Player("anyName", new Hand(cards)));
     }
 }
