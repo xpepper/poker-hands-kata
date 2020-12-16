cf903fe8e90420389f0c68749b5fa632dc96d136
Matteo Pierro <pierromatteo@gmail.com>
2020-12-16 23:16:16 +0200

    test GameRunner with 4 cards

--- src/test/java/com/kata/poker/GameRunnerTest.java
+++ src/test/java/com/kata/poker/GameRunnerTest.java
@@ -11,21 +11,21 @@ public class GameRunnerTest {
 
     @Test
     public void white_wins_with_high_card() {
-        String gameResult = gameRunner.run("Black: 2H 4D 6C  White: 2C 5H 7D");
+        String gameResult = gameRunner.run("Black: 2H 4D 6C 3H  White: 2C 5H 7D 6H");
 
         assertEquals("White wins. - with high card: 7", gameResult);
     }
 
     @Test
     public void black_wins_with_high_card() {
-        String gameResult = gameRunner.run("Black: 2C 5H 7D  White: 2H 4D 6C");
+        String gameResult = gameRunner.run("Black: 2C 5H 7D 6H  White: 2H 4D 6C 3H");
 
         assertTrue(gameResult.startsWith("Black wins. - with high card"));
     }
 
     @Test
     public void when_no_player_has_a_winning_hand_the_game_is_tie() {
-        String gameResult = gameRunner.run("Black: 2H 5D 7C  White: 2C 5H 7H");
+        String gameResult = gameRunner.run("Black: 2H 5D 7C 6H  White: 2C 5H 7H 3H");
 
         assertEquals("Tie.", gameResult);
     }
