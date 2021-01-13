375a355d2981b5744ebe5d252e63a0851036b3da
Matteo Pierro <pierromatteo@gmail.com>
2021-01-13 22:53:04 +0200

    #getPairs returns an optional

--- src/main/java/com/kata/poker/TwoPairsRule.java
+++ src/main/java/com/kata/poker/TwoPairsRule.java
@@ -1,6 +1,7 @@
 package com.kata.poker;
 
 import java.util.List;
+import java.util.Optional;
 
 import static java.util.Arrays.asList;
 
@@ -20,17 +21,17 @@ public class TwoPairsRule implements Rule {
 
     @Override
     public Rank apply(Hand hand) {
-        List<TwoCards> twoPairs = getTwoPairs(hand);
+        List<TwoCards> twoPairs = getTwoPairs(hand).get();
 
         return Rank.twoPairs(twoPairs.get(0).first().value, twoPairs.get(1).first().value);
     }
 
-    private List<TwoCards> getTwoPairs(Hand hand) {
+    private Optional<List<TwoCards>> getTwoPairs(Hand hand) {
         TwoCards firstPair = hand.selectTwoCardsWithTheSameValue().get();
         Cards otherCards = hand.allCardsExcept(firstPair.first(), firstPair.second());
         List<Card> otherPairCards = otherCards.selectCardsWithTheSameValue(2).get();
         TwoCards secondPair = new TwoCards(otherPairCards.get(0), otherPairCards.get(1));
         List<TwoCards> twoPairs = asList(firstPair, secondPair);
-        return twoPairs;
+        return Optional.of(twoPairs);
     }
 }
